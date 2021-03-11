956fad84ee8fb19d410f2f469734155b32427615
ray.chang <karta0898098@gmail.com>
2021-03-11 11:53:20 +0800

    fix typo

--- pkg/ratelimiter/domain/ratelimiter.go
+++ pkg/ratelimiter/domain/ratelimiter.go
@@ -13,6 +13,6 @@ type Claims struct {
 type RateLimiterService interface {
 	// RequireResource require access resource
 	// if too many request will return error
-	// must input addr because addr is unique key to identity witch ip income
+	// must input addr because addr is unique key to identity which ip income
 	RequireResource(ctx context.Context, addr string, url string) (claims *Claims, err error)
 }
